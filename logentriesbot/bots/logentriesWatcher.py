86e89c4dcdc77a4387a850985bbd13e14c4ce1fd
Vagner S <vagnervst17@gmail.com>
2018-02-08 00:20:23 -0200

    restore exec command

--- logentriesbot/bots/logentriesWatcher.py
+++ logentriesbot/bots/logentriesWatcher.py
@@ -1,4 +1,5 @@
 from logentriesbot.bots.bot import Bot
+from query import post_query, get_timestamp
 
 
 class LogWatcher(Bot):
@@ -7,12 +8,24 @@ class LogWatcher(Bot):
 
         self.commands = {
             "jump": self.jump,
+            "exec": self.exec,
             "help": self.help,
             "hello": self.hello
         }
 
     def jump(self, params=None):
         return "Kris Kross will make you jump jump"
+
+    def exec(self, command):
+        for c in command:
+            if c['name'] == 'query':
+                statement = c['value']
+            elif c['name'] == 'from':
+                from_time = c['value']
+            elif c['name'] == 'to':
+                to_time = c['value']
+
+        result = post_query(statement, from_time, to_time)
 
     def help(self, params=None):
         response = "Currently I support the following commands:\r\n"
